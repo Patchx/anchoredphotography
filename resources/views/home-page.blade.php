4e06ed1cda06b73900f0540fdae9e33b0868d754
Robert Anderson <robert.anderson.fl@gmail.com>
2020-11-08 19:24:06 -0500

    Removed the use of the 'container' class to simplify the template and make CSS changes easier to make

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -142,143 +142,84 @@
         </div>
     </div>
 
-    <div 
-        id="about-us"
-        class="container-fluid fh5co-about-us" 
-    >
-        <div class="container">
+    <div style="padding:50px">
+        <div id="about-us">
             <div class="row">
-                <div class="col-lg-4">
+                <div>
+                    <br>
+                    <h2>ABOUT US</h2>
 
-                    <svg class="back-bg" viewBox="0 0 535 618">
-                        <defs>
-                            <linearGradient 
-                                id="PSgrad_0" 
-                                x1="0%" 
-                                x2="100%" 
-                                y1="0%" 
-                                y2="0%"
-                            >
-                                <stop 
-                                    offset="0%" 
-                                    stop-color="rgb(88,192,255)" 
-                                    stop-opacity="1"
-                                />
-                                
-                                <stop 
-                                    offset="100%" 
-                                    stop-color="rgb(18,112,228)" 
-                                    stop-opacity="1"
-                                />
-                            </linearGradient>
-                        </defs>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed
+                        ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+                        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+                        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
+                        dolore magnam aliquam quaerat voluptatem.</p>
 
-                        <path fill-rule="evenodd" fill="rgb(18, 112, 228)"
-                            d="M6.000,-0.000 L499.000,-0.000 C502.314,-0.000 505.000,2.686 505.000,6.000 L505.000,612.000 C505.000,615.314 502.314,618.000 499.000,618.000 L6.000,618.000 C2.686,618.000 -0.000,615.314 -0.000,612.000 L-0.000,6.000 C-0.000,2.686 2.686,-0.000 6.000,-0.000 Z" />
-                        
-                        <path fill="url(#PSgrad_0)"
-                            d="M6.000,-0.000 L499.000,-0.000 C502.314,-0.000 505.000,2.686 505.000,6.000 L505.000,612.000 C505.000,615.314 502.314,618.000 499.000,618.000 L6.000,618.000 C2.686,618.000 -0.000,615.314 -0.000,612.000 L-0.000,6.000 C-0.000,2.686 2.686,-0.000 6.000,-0.000 Z" />
-                    </svg>
-
-                    <div class="owl-carousel owl-carousel1 owl-theme">
-                        <div>
-                            <img 
-                                src="/images/site_images/slide.png" 
-                                alt="" 
-                                class="img-fluid"
-                            />
-                        </div>
-
-                        <div>
-                            <img 
-                                src="/images/site_images/slide.png" 
-                                alt="" 
-                                class="img-fluid"
-                            />
-                        </div>
-
-                        <div>
-                            <img 
-                                src="/images/site_images/slide.png" 
-                                alt="" 
-                                class="img-fluid"
-                            />
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-8">
-                    <div class="about-us-content">
-                        <h2>ABOUT US</h2>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed
-                            ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-                            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-                            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-                            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-                            consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-                            dolore magnam aliquam quaerat voluptatem.</p>
-
-                        <a href="#" class="read-more">Read More</a>
-                    </div>
+                    <a href="#" class="read-more">Read More</a>
                 </div>
             </div>
         </div>
-    </div>
 
+        <div id="portfolio">
+            <div class="row">
+                <h2 style="margin-bottom:20px">OUR PORTFOLIO</h2>
 
-    <div class="container-fluid fh5co-portfolio" id="portfolio">
-        <div class="container">
-            <h2>OUR PORTFOLIO</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad </p>
-            <h3>FILTER BY:</h3>
-            <div class="portfolio-tab portfolioFilter">
-                <ul>
-                    <li><a href="#" data-filter="*" class="active">ALL</a>
-                    </li>
-                    <li><a href="#" data-filter=".landscape">landscape</a>
-                    </li>
-                    <li><a href="#" data-filter=".green">Green</a>
-                    </li>
-                    <li><a href="#" data-filter=".building">Building</a>
-                    </li>
-                    <li><a href="#" data-filter=".wedding">Weeding</a>
-                    </li>
-                    <li><a href="#" data-filter=".nature">Nature</a>
-                    </li>
-                    <li><a href="#" data-filter=".landscape">landscape</a>
-                    </li>
-                    <li><a href="#" data-filter=".green">Green</a>
-                    </li>
-                    <li><a href="#" data-filter=".building">Building</a>
-                    </li>
-                    <li><a href="#" data-filter=".wedding">Weeding</a>
-                    </li>
-                    <li><a href="#" data-filter=".nature">Nature</a>
-                    </li>
-                    <li><a href="#" data-filter=".landscape">landscape</a>
-                    </li>
-                    <li><a href="#" data-filter=".green">Green</a>
-                    </li>
-                    <li><a href="#" data-filter=".building">Building</a>
-                    </li>
-                    <li><a href="#" data-filter=".wedding">Weeding</a>
-                    </li>
-                    <li><a href="#" data-filter=".nature">Nature</a>
-                    </li>
-                    <li><a href="#" data-filter=".landscape">landscape</a>
-                    </li>
-                    <li><a href="#" data-filter=".green">Green</a>
-                    </li>
-                    <li><a href="#" data-filter=".building">Building</a>
-                    </li>
-                </ul>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad</p>
+
+                <div style="height:40px"></div>
+
+                <h3>FILTER BY:</h3>
+                
+                <div class="portfolio-tab portfolioFilter">
+                    <ul>
+                        <li><a href="#" data-filter="*" class="active">ALL</a>
+                        </li>
+                        <li><a href="#" data-filter=".landscape">landscape</a>
+                        </li>
+                        <li><a href="#" data-filter=".green">Green</a>
+                        </li>
+                        <li><a href="#" data-filter=".building">Building</a>
+                        </li>
+                        <li><a href="#" data-filter=".wedding">Weeding</a>
+                        </li>
+                        <li><a href="#" data-filter=".nature">Nature</a>
+                        </li>
+                        <li><a href="#" data-filter=".landscape">landscape</a>
+                        </li>
+                        <li><a href="#" data-filter=".green">Green</a>
+                        </li>
+                        <li><a href="#" data-filter=".building">Building</a>
+                        </li>
+                        <li><a href="#" data-filter=".wedding">Weeding</a>
+                        </li>
+                        <li><a href="#" data-filter=".nature">Nature</a>
+                        </li>
+                        <li><a href="#" data-filter=".landscape">landscape</a>
+                        </li>
+                        <li><a href="#" data-filter=".green">Green</a>
+                        </li>
+                        <li><a href="#" data-filter=".building">Building</a>
+                        </li>
+                        <li><a href="#" data-filter=".wedding">Weeding</a>
+                        </li>
+                        <li><a href="#" data-filter=".nature">Nature</a>
+                        </li>
+                        <li><a href="#" data-filter=".landscape">landscape</a>
+                        </li>
+                        <li><a href="#" data-filter=".green">Green</a>
+                        </li>
+                        <li><a href="#" data-filter=".building">Building</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
             <div class="row">
@@ -445,10 +386,9 @@
             </div>
         </div>
 
-    </div>
-    <div class="container-fluid fh5co-news" id="news">
-        <div class="container">
+        <div class="container-fluid fh5co-news" id="news">
             <h2>OUR NEWS</h2>
+
             <div class="row">
                 <div class="owl-carousel owl-carousel2 owl-theme">
                     <div>
@@ -489,94 +429,94 @@
                 </div>
             </div>
         </div>
-    </div>
 
-    <footer class="container-fluid fh5co-footer">
-        <div class="container" id="contact">
-            <div class="row">
-                <div class="col-lg-5">
-                    <h2>LET'S CHAT!</h2>
-                    
-                    <p>
-                        <span class="email">
-                            <img 
-                                src="/images/site_images/email.png"
-                                alt="email icon"
-                            />
-                        </span>
+        <footer class="container-fluid fh5co-footer">
+            <div id="contact">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <h2>LET'S CHAT!</h2>
+                        
+                        <p>
+                            <span class="email">
+                                <img 
+                                    src="/images/site_images/email.png"
+                                    alt="email icon"
+                                />
+                            </span>
 
-                        <b><a href="mailto:anchoredphotographyFL@gmail.com">
-                                anchoredphotographyFL@gmail.com
-                            </a>
-                        </b>
-                    </p>
-                    
-                    <p>
-                        <span class="phone">
-                            <img 
-                                src="/images/site_images/phone.png" 
-                                alt="phone icon"
-                            />
-                        </span>
+                            <b><a href="mailto:anchoredphotographyFL@gmail.com">
+                                    anchoredphotographyFL@gmail.com
+                                </a>
+                            </b>
+                        </p>
+                        
+                        <p>
+                            <span class="phone">
+                                <img 
+                                    src="/images/site_images/phone.png" 
+                                    alt="phone icon"
+                                />
+                            </span>
 
-                        <b><a href="tel:561-502-9176">
-                            561-502-9176
-                        </a></b>
-                    </p>
-                                        
-                    <ul class="navbar-nav float-left social-links footer-social">
-                        <li class="nav-item">
-                            <a 
-                                class="nav-link" 
-                                href="https://facebook.com/AnchoredPhotographybyRachel"
-                            >
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                        </li>
+                            <b><a href="tel:561-502-9176">
+                                561-502-9176
+                            </a></b>
+                        </p>
+                                            
+                        <ul class="navbar-nav float-left social-links footer-social">
+                            <li class="nav-item">
+                                <a 
+                                    class="nav-link" 
+                                    href="https://facebook.com/AnchoredPhotographybyRachel"
+                                >
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
 
-                        <li class="nav-item">
-                            <a 
-                                class="nav-link" 
-                                href="https://instagram.com/anchoredphotographyFL"
-                            >
-                                <i class="fab fa-instagram"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                            <li class="nav-item">
+                                <a 
+                                    class="nav-link" 
+                                    href="https://instagram.com/anchoredphotographyFL"
+                                >
+                                    <i class="fab fa-instagram"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
 
-                <div class="col-lg-7">
-                    <div class="form-box">
-                        <h4 style="margin-left:20px">Contact Me</h4>
-                        <hr />
+                    <div class="col-lg-7">
+                        <div class="form-box">
+                            <h4 style="margin-left:20px">Contact Me</h4>
+                            <hr />
 
-                        <table class="table table-light table-borderless">
-                            <tr>
-                                <td><input type="text" class="form-control" placeholder="Name...">
-                                </td>
+                            <table class="table table-light table-borderless">
+                                <tr>
+                                    <td><input type="text" class="form-control" placeholder="Name...">
+                                    </td>
 
-                                <td><input type="text" class="form-control" placeholder="Email address">
-                                </td>
-                            </tr>
+                                    <td><input type="text" class="form-control" placeholder="Email address">
+                                    </td>
+                                </tr>
 
-                            <tr>
-                                <td colspan="2"><textarea class="form-control" placeholder="You Message"></textarea>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="2">
-                                    <button type="submit">
-                                        SUBMIT NOW
-                                    </button>
+                                <tr>
+                                    <td colspan="2"><textarea class="form-control" placeholder="You Message"></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <button type="submit">
+                                            SUBMIT NOW
+                                        </button>
 
-                                </td>
-                            </tr>
-                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
+    </div>
 
     <div class="container-fluid copy">
         <div class="col-lg-12">
