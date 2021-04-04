61fb99b1021cc1a49c6b11841e6e8307666a74fe
Robert Anderson <robert.anderson.fl@gmail.com>
2021-04-04 14:45:32 -0400

    Converted the 'Book Now' page to use the main marketing layout

--- resources/views/calendar/schedule.blade.php
+++ resources/views/calendar/schedule.blade.php
@@ -1,13 +1,25 @@
-@extends('layouts.admin-layout')
+@extends('layouts.marketing-layout')
 
 @section('content')
-<h1 class="text-center">Book a Time with Me</h1>
+	<div style="padding:25px">
+    	<h1 class="text-center">Book a Time with Me</h1>
 
-<div>
-	<!-- Calendly inline widget begin -->
-	<div class="calendly-inline-widget" data-url="https://calendly.com/robert-anderson-fl" style="min-width:320px;height:630px;"></div>
-	<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
-	<!-- Calendly inline widget end -->
-</div>
-
+    	<div>
+    		<!-- Calendly inline widget begin -->
+    		<div 
+    			class="calendly-inline-widget" 
+    			data-url="https://calendly.com/robert-anderson-fl" 
+    			style="
+    				min-width: 320px;
+    				height: 650px;
+    			"
+    		></div>
+    		
+    		<script 
+    			type="text/javascript" 
+    			src="https://assets.calendly.com/assets/external/widget.js"
+    		></script>
+    		<!-- Calendly inline widget end -->
+    	</div>
+	</div>
 @endsection
