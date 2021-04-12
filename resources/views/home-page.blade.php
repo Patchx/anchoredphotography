305817864ad421f0d99da233dbc1cb07712a1c9b
Robert Anderson <robert.anderson.fl@gmail.com>
2021-04-11 20:57:02 -0400

    Added the gallery preview cards to the main landing page

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -1,31 +1,6 @@
 @extends('layouts.marketing-layout')
 
 @section('head-unique')
-
-    <style>
-        .testimonial {
-            text-align: center;
-            padding: 45px 50px 45px 70px;
-            margin: 30px 15px 35px;
-            background: #f9f9f9;
-            position: relative;
-        }
-        .testimonial .description {
-            font-size: 20px;
-            color: #757575;
-            line-height: 27px;
-            margin-bottom: 20px;
-            position: relative;
-        }
-        .testimonial .title {
-            display: inline-block;
-            font-size: 18px;
-            color: #4a5184;
-            letter-spacing: 1px;
-            text-transform: uppercase;
-            margin: 0;
-        }
-    </style>
 @endsection
 
 @section('content')
@@ -41,13 +16,7 @@
         @endforeach
     </div>
 
-    <div 
-        class="mx-auto"
-        style="
-            padding: 40px;
-            max-width: 700px;
-        "
-    >
+    <div class="main-content-padding mx-auto">
         <div class="row">
             <br>
             <h2
@@ -57,9 +26,75 @@
 
             <p>I am a natural light photographer based on the Treasure Coast of Florida. I’m a native Floridian who grew up in South Florida, so I know the area well... which is always a plus! I love photographing everything from engagements to maternity, family lifestyle sessions to your babies graduating and everything in between! I have a passion for creating relationships with my clients and bringing their visions to life. There are so many amazing milestones in life, and I feel super grateful that my clients trust me to preserve those moments through photography.</p>
         </div>
+    </div>
 
-        <div class="row">
-            <div id="testimonial-slider">
+    <div class="mint-light-green-bg">
+        <div class="main-content-padding mx-auto">
+            <div>
+                <h2 class="gallery-preview-title">Anniversaries</h2>
+
+                <div
+                    class="gallery-preview-image-div"
+                    style="
+                        background: center no-repeat url('https://res.cloudinary.com/lessondb/image/upload/f_auto,q_auto:eco,g_auto/v1615759785/anchoredphotographyfl/wedding_1.jpg');
+                        background-size: cover;
+                    "
+                /></div>
+            </div>
+
+            <div>
+                <h2 class="gallery-preview-title">Maternity</h2>
+
+                <div
+                    class="gallery-preview-image-div"
+                    style="
+                        background: center no-repeat url('https://res.cloudinary.com/lessondb/image/upload/f_auto,q_auto:eco,g_auto/v1615768165/anchoredphotographyfl/maternity_1.jpg');
+                        background-size: cover;
+                    "
+                /></div>
+            </div>
+
+            <div>
+                <h2 class="gallery-preview-title">Individuals & Couples</h2>
+
+                <div
+                    class="gallery-preview-image-div"
+                    style="
+                        background: center no-repeat url('https://res.cloudinary.com/lessondb/image/upload/f_auto,q_auto:eco,g_auto/v1615759769/anchoredphotographyfl/family_3.jpg');
+                        background-size: cover;
+                    "
+                /></div>
+            </div>
+
+            <div>
+                <h2 class="gallery-preview-title">Family Portraits</h2>
+
+                <div
+                    class="gallery-preview-image-div"
+                    style="
+                        background: center no-repeat url('https://res.cloudinary.com/lessondb/image/upload/f_auto,q_auto:eco,g_auto/v1615768144/anchoredphotographyfl/kids_1.jpg');
+                        background-size: cover;
+                    "
+                /></div>
+            </div>
+
+            <div>
+                <h2 class="gallery-preview-title">Engagement</h2>
+
+                <div
+                    class="gallery-preview-image-div"
+                    style="
+                        background: center no-repeat url('https://res.cloudinary.com/lessondb/image/upload/f_auto,q_auto:eco,g_auto/v1615759827/anchoredphotographyfl/ring_1.jpg');
+                        background-size: cover;
+                    "
+                /></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="rose-bg">
+        <div class="main-content-padding mx-auto">
+            <div class="row">
                 <div class="testimonial">
                     <p class="description">
                         Rachel is great! But this is just example text!
@@ -68,7 +103,7 @@
                         <h3 class="title">John Doe</h3>
                     </div>
                 </div>
- 
+
                 <div class="testimonial">
                     <p class="description">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto commodi dolorum earum fugiat, fugit hic id, ipsum laborum minus nostrum numquam perspiciatis saepe velit.
@@ -77,7 +112,7 @@
                         <h3 class="title">Kristina</h3>
                     </div>
                 </div>
- 
+
                 <div class="testimonial">
                     <p class="description">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto commodi dolorum earum fugiat, fugit hic id, ipsum laborum minus nostrum numquam perspiciatis saepe velit.
@@ -89,6 +124,7 @@
             </div>
         </div>
     </div>
+    <br><br>
 
     <div 
         id="carouselExampleControls" 
@@ -100,7 +136,7 @@
         <div class="carousel-inner">
             @foreach($bottom_image_urls as $url)
                 <div 
-                    @if ($loop->first)
+                    @if($loop->first)
                         class="carousel-item active"
                     @else
                         class="carousel-item"
